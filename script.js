655ce54e3d2c84083a8469d972662223335b04a8
abhishekkr523 <7033abhishek@gmail.com>
2024-03-24 18:51:18 +0530

    resolve some issue in toggle and css

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-window.onload = function(){
-    const sidebar = document.querySelector(".sidebar");
-    const closeBtn = document.querySelector("#btn");
-    const searchBtn = document.querySelector(".bx-search")
+document.addEventListener('DOMContentLoaded', function () {
+    // Add click event listeners to all links inside the navigation list
+    var navLinks = document.querySelectorAll('.nav-list a');
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            // Remove 'active' class from all links
+            navLinks.forEach(function (link) {
+                link.classList.remove('active');
+            });
+            // Add 'active' class to the clicked link
+            link.classList.add('active');
 
-    closeBtn.addEventListener("click",function(){
-        sidebar.classList.toggle("open")
-        menuBtnChange()
-    })
+            // Check if the screen width is less than or equal to 767px
+            if (window.innerWidth <= 767) {
+                // Hide the navigation list when a link is clicked
+                var navList = document.querySelector('.nav-list');
+                var toggleButton1 = document.querySelector('.toggle-button i.fa-bars');
+                var toggleButton2 = document.querySelector('.toggle-button i.fa-xmark');
+                navList.style.display = 'none';
+                toggleButton1.style.display = 'block';
+                toggleButton2.style.display = 'none';
+            }
+        });
+    });
 
-    searchBtn.addEventListener("click",function(){
-        sidebar.classList.toggle("open")
-        menuBtnChange()
-    })
 
-    function menuBtnChange(){
-        if(sidebar.classList.contains("open")){
-            closeBtn.classList.replace("bx-menu","bx-menu-alt-right")
-        }else{
-            closeBtn.classList.replace("bx-menu-alt-right","bx-menu")
-        }
-    }
+    
+
+});
+function toggleMenu1() {
+    var navList = document.querySelector('.nav-list');
+    navList.style.display = 'flex';
+    navList.style.flexDirection = 'column'; // Set flex-direction to column
+    var toggleButton1 = document.querySelector('.toggle-button i.fa-bars');
+    var toggleButton2 = document.querySelector('.toggle-button i.fa-xmark');
+    toggleButton1.style.display = 'none';
+    toggleButton2.style.display = 'block';
 }
+
+function toggleMenu2() {
+    var navList = document.querySelector('.nav-list');
+    navList.style.display = 'none';
+    var toggleButton1 = document.querySelector('.toggle-button i.fa-bars');
+    var toggleButton2 = document.querySelector('.toggle-button i.fa-xmark');
+    toggleButton1.style.display = 'block';
+    toggleButton2.style.display = 'none';
+}
+
+
